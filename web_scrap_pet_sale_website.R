b101e7a1237321136d7d144fba1e05206f47d23b
Angus Chan <123187295+cyfangus@users.noreply.github.com>
2024-01-12 12:24:44 +0000

    Update web_scrap_pet_sale_website.R

--- web_scrap_pet_sale_website.R
+++ web_scrap_pet_sale_website.R
@@ -1,3 +1,5 @@
+## This stores the R codes doing web scrapping on a pet sale website and generating a dataset with listing names, prices, details, and locations (stored as csv).
+
 library(rvest)
 library(stringr)
 # Scraping listing name, and the auction details
